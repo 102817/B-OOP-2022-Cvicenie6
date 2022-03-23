82d53a244ccba35bddda9bf88fb580375aab71e2
Viktoriia Fedynaik <xfedyniak@stuba.sk>
2022-03-23 16:17:13 +0100

    frame

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -11,6 +11,23 @@ public class Main {
         okno.setResizable(false);  //fiksuje velkost okna
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //Ked stlacim krizik tak skonci program
         okno.setLayout(new GridLayout(3,4)); //zvacsuje zmensujje velkost okna
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5,5));
+
+
+
+        panel.add(new JLabel("TOP"), BorderLayout.PAGE_START);
+        panel.add(new JLabel("BOTTOM"), BorderLayout.PAGE_END);
+        panel.add(new JLabel("LEFT"), BorderLayout.PAGE_START);
+        panel.add(new JLabel("Rigth"), BorderLayout.PAGE_END);
+        JLabel labelPanel = new JLabel("CENTER", SwingConstants.CENTER);
+        labelPanel.setAlignmentX(50);
+        panel.add(labelPanel, BorderLayout.CENTER);
+
+        panel.setBackground(new Color(190, 120, 150));
+        okno.getContentPane().setBackground(Color.YELLOW);
+
         JLabel label = new JLabel("Hello world");
         okno.add(label);
 
@@ -18,7 +35,14 @@ public class Main {
             okno.add(new JLabel("Hello " + i));
 
         }
+        for (int i = 0;i<10;i++) {
 
+            if (i == 8) {
+                okno.add(panel);
+            } else{
+                okno.add(new JLabel("Hello " + i));
+            }
+        }
 
     }
 }
